9cd802178c129fd616dd82852280bc5190802685
Code Sandbox <hello@codesandbox.io>
2022-05-29 13:02:05 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.fbd83ad3531e665ce69633732b8617b5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30edec2de97ecf6503c63801c074da3",
+    "url": "https://artmirror2000.github.io/csb-qvc3fr/index.html"
+  },
+  {
+    "revision": "c969a4eaf285064cd450",
+    "url": "https://artmirror2000.github.io/csb-qvc3fr/static/js/2.71946958.chunk.js"
+  },
+  {
+    "revision": "3f807637e05ec5fce973",
+    "url": "https://artmirror2000.github.io/csb-qvc3fr/static/js/main.58bdd9c0.chunk.js"
+  },
+  {
+    "revision": "67fd8f24c9ab4762a27d",
+    "url": "https://artmirror2000.github.io/csb-qvc3fr/static/js/runtime-main.99202a87.js"
+  }
+]);
